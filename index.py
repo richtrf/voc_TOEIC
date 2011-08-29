14fe9e7c7a9740b750febe29823e0a49559e39ab
Richard Y. Chang <richtrf@gmail.com>
2011-08-29 20:16:02 +0800

    Initial

---
+++ index.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+import sqlite3
+
+
+def input(request):
+    return render_to_response("input.html")
+
+def generate(request):
+    rannum=request.POST['itemno']
+    
+    ## SQLITE3 ##
+    db=sqlite3.connect("toeic.db")
+    cursor=db.cursor()
+    cursor.execute("SELECT A,B,C,D,item,Ans FROM dist ORDER BY RANDOM() LIMIT %s" % (rannum))
+    r=cursor.fetchall()
+    db.close
+    r=[(i1.split(';')[0],i2.split(';')[0],i3.split(';')[0],i4.split(';')[0],example,Ans) for i1,i2,i3,i4, example,Ans in r]
+    return render_to_response("item.html", {'item':r})
